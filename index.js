266705c2cfb59d23934105008e50438f3aa9bb3f
Chenai Chambers <chambers.t@hotmail.co.uk>
2020-07-01 19:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays() {
 function happyHolidaysTo(Chenai) {
   return `Happy holidays, ${Chenai}!`
 }
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
